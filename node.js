40f6be41dce79418b82a6d53dfdb9e08f7fef121
NgDuyTho <nguyenduytho18@gmail.com>
2022-03-28 08:21:26 +0700

    add

---
+++ node.js
@@ -0,0 +1,21 @@
+let a =2;
+
+ if(a%2==0)
+ {
+     console.log("số chẵn");
+ }else{
+     console.log("số lẻ");
+ }
+
+ for(let i=0;i<=10;i++)
+ {
+     console.log(i);
+ }
+ function myFunction() {
+     a=1500;
+     b=100;
+     c=a/b;
+     console.log(c);
+     return c;
+  }
+  
